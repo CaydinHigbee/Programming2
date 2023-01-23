a4ea8bee5d172e314054a2a0e5cd524763430cd0
CaydinHigbee <112713167+CaydinHigbee@users.noreply.github.com>
2023-01-23 14:37:31 +0000

    math

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-while True:
-  print("HeLlO wOrLd!")
+print("HeLlO wOrLd!")
+# This is a single line comment
+
+""" Arithmatic Operators
++ Add
+- Subtract
+* Multiply
+/ Divide
+// Floor Divide (Round down to the nearest int)
+% Mod ( Modulus/Remainder from Int division)
+** Pow
+"""
+
+"""Assignment Opperators
+= value assignment (x = y)
++= Add to (x += y is shothand for x = x + y)
+-= Subtract From (x -= y is shorthand for x = x - y)
+*=
+/=
+//=
+%=
+**=
+"""
+
+"""Data Types
+int - Integer (whole numeber)
+float - floating-point number (number w/ a decimal)
+str - String (any text in quotes)
+bool - Boolean (either True or False only)
+chr - Character (single keyboard character, in single-quotes)
+"""
---
+++ prog52a.py
@@ -0,0 +1,7 @@
+# Lang 52a - Area aand Perimeter of rectangle
+length = int(input("Enter Length: "))
+width = int(input("Enter Width: "))
+area = length * width
+perim = 2 * length + 2* width
+print("Area =", area)
+print("perimeter = " + str(perim))
